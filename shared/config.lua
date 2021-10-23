d5ff2a3912886f7f91de6d27ef6d8a6cefd4112c
Fellow25 <92899080+Fellow25@users.noreply.github.com>
2021-10-23 12:11:48 +0100

    Add files via upload

---
+++ shared/config.lua
@@ -0,0 +1,62 @@
+Ammujob             = {}
+Ammujob.jeveuxmarker = true --- true = Oui | false = Non
+
+Ammujob.pos = {
+	coffre = {
+		position = {x = 814.02, y = -2153.04, z = 29.62}
+	},
+	garage = {
+		position = {x = 822.06, y = -2143.55, z = 28.81}
+	},
+	spawnvoiture = {
+		position = {x = 822.06, y = -2143.55, z = 28.81, h = 358.734}
+	},
+	boss = {
+		position = {x = 825.97, y = -2150.94, z = 29.62}
+	},
+	blips = {
+		position = {x = 811.93, y = -2147.55, z = 29.5}
+	},
+	theorie = {
+		position = {x = 807.85, y = -2159.37, z = 29.62}
+	},
+	craftmenu = {
+		position = {x = 808.97, y = -2172.92, z = 29.62}
+	},
+}
+
+Ammujob.farm = {
+	metaux = {
+		position = {x = 991.63, y = -2430.95, z = 31.24}
+	},
+	meche = {
+		position = {x = 2461.51, y = 1589.32, z = 33.0}
+	},
+	canon = {
+		position = {x = 2178.49, y = 3497.34, z = 45.39}
+	},
+	levier = {
+		position = {x = -1933.53, y = 2039.41, z = 140.83}
+	}
+}
+
+Ammujob.craft = {
+	{name = "Batte de baseball", weapon = "bat", metaux = 1, canon = 0, meche = 0, levier = 0},
+	{name = "Poing américain", weapon = "knuckle", metaux = 5, canon = 0, meche = 0, levier = 0},
+	{name = "Couteau", weapon = "switchblade", metaux = 15, canon = 0, meche = 5, levier = 0},
+	{name = "Pistolet", weapon = "pistol", metaux = 25, canon = 1, meche = 1, levier = 1},
+	{name = "Revolver", weapon = "revolver", metaux = 50, canon = 2, meche = 2, levier = 2},
+	{name = "Mini SMG", weapon = "minismg", metaux = 100, canon = 10, meche = 10, levier = 10},
+	{name = "SMG", weapon = "smg", metaux = 150, canon = 15, meche = 15, levier = 15},
+	{name = "Fusil à pompe", weapon = "pumpshotgun", metaux = 200, canon = 20, meche = 20, levier = 20},
+	{name = "Fusil d'assaut", weapon = "assaultrifle", metaux = 250, canon = 45, meche = 45, levier = 45},
+	{name = "Carabine compacte", weapon = "compactrifle", metaux = 500, canon = 90, meche = 90, levier = 90},
+	{name = "Munitions pistolet", weapon = "pistol_ammo", metaux = 15, canon = 0, meche = 0, levier = 0},
+	{name = "Munitions d'assaut", weapon = "rifle_ammo", metaux = 30, canon = 0, meche = 0, levier = 0},
+	{name = "Cartouche", weapon = "shotgun_ammo", metaux = 45, canon = 0, meche = 0, levier = 0},
+	{name = "Munitions SMG", weapon = "smg_ammo", metaux = 60, canon = 0, meche = 0, levier = 0}
+}
+
+GAmmuvoiture = {
+    {nom = "Stockade", modele = "stockade"},
+}
